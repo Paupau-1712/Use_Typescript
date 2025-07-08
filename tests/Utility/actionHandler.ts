3d7fc73ab789688aa9f77e3d9687e004b2a1ad7c
Paul Anthony <76605759+Paupau-1712@users.noreply.github.com>
2025-07-08 19:52:04 +0800

    Counting Sheets

--- tests/Utility/actionHandler.ts
+++ tests/Utility/actionHandler.ts
@@ -14,29 +14,16 @@ export class actionHandler {
     }
   }
 
-async getFormattedTimestamp(): Promise<string> {
-  const now = new Date();
-  const yyyy = now.getFullYear();
-  const mm = String(now.getMonth() + 1).padStart(2, '0');
-  const dd = String(now.getDate()).padStart(2, '0');
-  const hh = String(now.getHours()).padStart(2, '0');
-  const min = String(now.getMinutes()).padStart(2, '0');
-  const ss = String(now.getSeconds()).padStart(2, '0');
-  return `${yyyy}-${mm}-${dd}_${hh}-${min}-${ss}`;
-}
-
-
 
   async performAction(step: number, elementType: string, locatorPath: string, locatorValue: string) {
     const locators = this.locator.getLocator(locatorPath);
 
     try {
-      switch (elementType) {
+      switch (elementType.toLocaleLowerCase()) {
 
         case 'URL':
           await this.page.goto(locatorValue);   // ✅ Open a generic URL
           break;
-
         case 'button':
           await locators.click();   // ✅ Click a generic button
           break;
@@ -47,12 +34,13 @@ async getFormattedTimestamp(): Promise<string> {
         case 'closeBrowser':
           await this.page.close();
           break;
+
         // Validations
-        case 'waitforURL':
+        case 'waitforURLtolload':
           await this.page.waitForURL;   // ✅ waitforURL to load
           break;
       }
-      await this.page.screenshot({ path: `Test Step/${this.getFormattedTimestamp}-Step${step}.png` });
+      await this.page.screenshot({ path: `Test Step/${Date.now()}-Step${step}.png` });
 
 
     } catch (error) {
